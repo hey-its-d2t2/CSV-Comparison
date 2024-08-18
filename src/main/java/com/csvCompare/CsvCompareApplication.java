9d8635505c0d5d367875b99166e6d426c32618b1
Deepak Singh <deepsinghkumar01@gmail.com>
2024-08-18 17:55:56 +0530

    Initial commit,project created...

---
+++ src/main/java/com/csvCompare/CsvCompareApplication.java
@@ -0,0 +1,13 @@
+package com.csvCompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvCompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csvCompare/CsvCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csvCompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
